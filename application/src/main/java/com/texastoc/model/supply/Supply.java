10a474370a52f0f5f56011518cf71941ec0ca675
Gil Pratte <gil.pratte@mutualmobile.com>
2018-12-31 14:37:21 -0600

    TDD JUnit development for SupplyService

--- application/src/main/java/com/texastoc/model/supply/Supply.java
+++ application/src/main/java/com/texastoc/model/supply/Supply.java
@@ -1,18 +1,22 @@
 package com.texastoc.model.supply;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Supply {
+    private int id;
     private LocalDate date;
-    private String name;
-    private int cost;
-    private String description;
     private SupplyType type;
+    private int amount;
+    private String description;
 }
---
+++ application/src/main/java/com/texastoc/repository/SupplyRepository.java
@@ -0,0 +1,20 @@
+package com.texastoc.repository;
+
+import com.texastoc.model.supply.Supply;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class SupplyRepository {
+
+    public List<Supply> get() {
+        return null;
+    }
+
+    public void save(Supply supply) {
+    }
+
+    public void delete(int id) {
+    }
+}
