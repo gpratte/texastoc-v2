4015540bbec0d5f7a28a7f63c0be521dcec110d1
Gil Pratte <gil.pratte@mutualmobile.com>
2019-01-27 10:58:37 -0600

    procfile to running on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar application/target/texastoc-application-1.0.jar --server.port=$PORT --spring.profiles.active=test com.texastoc.Application
--- application/src/main/java/com/texastoc/config/SetupDatabase.java
+++ application/src/main/java/com/texastoc/config/SetupDatabase.java
@@ -4,12 +4,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.ResourceUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 /**
  * Only run if the spring profile is "test"
@@ -20,14 +24,15 @@ public class SetupDatabase {
     @Bean
     CommandLineRunner init(JdbcTemplate jdbcTemplate) {
         return args -> {
-            File file = ResourceUtils.getFile("classpath:create_toc_schema.sql");
-            try (BufferedReader bf = Files.newBufferedReader(file.toPath())) {
+            InputStream resource = new ClassPathResource(
+                "create_toc_schema.sql").getInputStream();
+            try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(resource))) {
                 String line;
-                while ((line = bf.readLine()) != null) {
+                while ((line = reader.readLine()) != null) {
                     jdbcTemplate.execute(line);
                 }
             }
         };
     }
-
 }
